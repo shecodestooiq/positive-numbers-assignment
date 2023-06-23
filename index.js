5fa956c1a6e410b3e210a77da90a1092135b8e65
sanguine-dreams <sara0dhahir@gmail.com>
2023-06-24 00:44:00 +0300

    change getPositiveSum function to ES6 syntax

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ class NumberArray {
     this.numbers = numbers;
   }
 
-  getPositiveSum() {
-    let sum = 0;
-    for (let number of this.numbers) {
+  getPositiveSum = () => {
+    return this.numbers.reduce((sum, number) => { //return of the function is the return of the reduce function 
       if (number > 0) {
-        sum += number;
+        return sum + number; //this return is that of the if statement which return the value to the reduce function to go again
       }
-    }
-    return sum;
+      return sum; //this is the return of the reduce function that feeds into the getPositiveSum's return 
+    }, 0);
   }
+
+  // getPositiveSum() {
+  //   let sum = 0;
+  //   for (let number of this.numbers) {
+  //     if (number > 0) {
+  //       sum += number;
+  //     }
+  //   }
+  //   return sum;
+  // }
 }
 
 const numbers = [2, -4, 6, -8, 10, -12];
@@ -20,3 +29,5 @@ const positiveSum = numberArray.getPositiveSum();
 
 // Input: [2, -4, 6, -8, 10, -12]
 // Output: 18
+
+console.log(positiveSum);
