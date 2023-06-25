5ea67366f53535287347d50c588377ed5c04063b
ulaali <ulaali513@yahoo.com>
2023-06-25 22:27:23 +0300

    implement positive sum method

--- index.js
+++ index.js
@@ -21,20 +21,20 @@
 // // Input: [2, -4, 6, -8, 10, -12]
 // // Output: 18
 
+
 function getPositiveSum(numbers){
   let sum = 0;
-    for (let number of numbers) {
-      if (number > 0) {
-        sum += number;
-      }
-    }
-    return sum;
-  }
+  return sum =numbers.reduce((total,index)=>{
+    if(index>0){
+    total+=index}
+    return total
+  },0)
+  
+}
 
 
 
 const numbers = [2, -4, 6, -8, 10, -12];
 
 console.log(getPositiveSum(numbers));
-
 module.exports=getPositiveSum;
