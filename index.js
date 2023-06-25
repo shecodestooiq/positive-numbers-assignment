7b4c80c63b97c8d2d822c7cbc0eea4cdced6ad34
Aya Jafar <103113441+Aya-Jafar@users.noreply.github.com>
2023-06-25 13:47:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,3 +20,38 @@ const positiveSum = numberArray.getPositiveSum();
 
 // Input: [2, -4, 6, -8, 10, -12]
 // Output: 18
+
+
+/* Functional  version*/
+function getPositiveSum(arr) {
+    let sum = 0;
+    for (let number of arr) {
+      if (number > 0) {
+        sum += number;
+      }
+    }
+    return sum;
+}
+console.log(getPositiveSum(numbers))
+
+
+/* Es6  version*/
+const arr = [...numbers]
+const getPositiveSumEs6 = (arr) => {
+    if(arr.length === 0) {
+      return 0;
+    }
+    let sum = 0;
+    for (let number of arr) {
+      if (number > 0) {
+        sum += number;
+      }
+    }
+    return sum
+}
+
+console.log(getPositiveSumEs6(arr))
+
+
+module.exports = {getPositiveSumEs6}
+
