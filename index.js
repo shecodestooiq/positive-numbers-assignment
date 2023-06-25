debdbb85690cdcc06c96bc797d03f2427566099e
yasminm97 <yassssssah@gmail.com>
2023-06-25 19:53:36 +0300

    change code into functional JS

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-class NumberArray {
-  constructor(numbers) {
-    this.numbers = numbers;
-  }
+// class NumberArray {
+//   constructor(numbers) {
+//     this.numbers = numbers;
+//   }
 
-  getPositiveSum() {
-    let sum = 0;
-    for (let number of this.numbers) {
-      if (number > 0) {
-        sum += number;
-      }
-    }
-    return sum;
-  }
-}
+//   getPositiveSum() {
+//     let sum = 0;
+//     for (let number of this.numbers) {
+//       if (number > 0) {
+//         sum += number;
+//       }
+//     }
+//     return sum;
+//   }
+// }
 
-const numbers = [2, -4, 6, -8, 10, -12];
-const numberArray = new NumberArray(numbers);
-const positiveSum = numberArray.getPositiveSum();
+// const numbers = [2, -4, 6, -8, 10, -12];
+// const numberArray = new NumberArray(numbers);
+// const positiveSum = numberArray.getPositiveSum();
 
 // Input: [2, -4, 6, -8, 10, -12]
 // Output: 18
+
+
+getPositiveSum = () => {
+  let sum = 0;
+  for (let number of numbers) {
+    if (number > 0) {
+      sum += number;
+    }
+  }
+  return sum;
+}
+const numbers = [2, -4, 6, -8, 10, -12];
+console.log(getPositiveSum(numbers));
