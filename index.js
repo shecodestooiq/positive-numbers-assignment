40706a471ff861fcb063e38a196f06bb2eee3d06
yasminm97 <yassssssah@gmail.com>
2023-06-25 20:07:44 +0300

    add a comment

--- index.js
+++ index.js
@@ -21,7 +21,6 @@
 // Input: [2, -4, 6, -8, 10, -12]
 // Output: 18
 
-
 getPositiveSum = () => {
   let sum = 0;
   for (let number of numbers) {
@@ -34,3 +33,9 @@ getPositiveSum = () => {
 module.exports = getPositiveSum;
 const numbers = [2, -4, 6, -8, 10, -12];
 console.log(getPositiveSum(numbers));
+
+
+// //Functions do specific things, classes are specific things.
+// Classes often have methods, which are functions that are associated with
+// a particular class, and do things associated with the thing that the class is -
+// but if all you want is to do something, a function is all you need.
