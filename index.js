adb69e34882fa283155784ff33daec8e75a9043e
ulaali <ulaali513@yahoo.com>
2023-06-25 20:59:42 +0300

    implement getpositiveSum function

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-class NumberArray {
-  constructor(numbers) {
-    this.numbers = numbers;
-  }
+// class NumberArray {
+//   constructor(numbers) {
+//     this.numbers = numbers;
+//   }
 
-  getPositiveSum() {
-    let sum = 0;
-    for (let number of this.numbers) {
+//   getPositiveSum() {
+//     let sum = 0;
+//     for (let number of this.numbers) {
+//       if (number > 0) {
+//         sum += number;
+//       }
+//     }
+//     return sum;
+//   }
+// }
+
+// const numbers = [2, -4, 6, -8, 10, -12];
+// const numberArray = new NumberArray(numbers);
+// const positiveSum = numberArray.getPositiveSum();
+
+// // Input: [2, -4, 6, -8, 10, -12]
+// // Output: 18
+
+function getPositiveSum(numbers){
+  let sum = 0;
+    for (let number of numbers) {
       if (number > 0) {
         sum += number;
       }
     }
     return sum;
   }
-}
+
+
 
 const numbers = [2, -4, 6, -8, 10, -12];
-const numberArray = new NumberArray(numbers);
-const positiveSum = numberArray.getPositiveSum();
 
-// Input: [2, -4, 6, -8, 10, -12]
-// Output: 18
+console.log(getPositiveSum(numbers));
