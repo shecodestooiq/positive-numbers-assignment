73cb1e1925af6f63159c68053accc36696a0760c
howraa9 <136803949+howraa9@users.noreply.github.com>
2023-06-25 21:57:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ class NumberArray {
     this.numbers = numbers;
   }
 
-  getPositiveSum() {
-    let sum = 0;
-    for (let number of this.numbers) {
-      if (number > 0) {
-        sum += number;
-      }
-    }
-    return sum;
+  //getPositiveSum() {
+   // let sum = 0;
+   // for (let number of this.numbers) {
+     // if (number > 0) {
+     //   sum += number;
+     // }
+   // }
+   // return sum;
+  //}
+//}
+getPositiveSum() {
+    return this.numbers.reduce((sum, number) => sum + (number > 0 ? number : 0), 0);
   }
 }
 
 const numbers = [2, -4, 6, -8, 10, -12];
 const numberArray = new NumberArray(numbers);
 const positiveSum = numberArray.getPositiveSum();
+
+//const numbers = [2, -4, 6, -8, 10, -12];
+//const numberArray = new NumberArray(numbers);
+//const positiveSum = numberArray.getPositiveSum();
 
 // Input: [2, -4, 6, -8, 10, -12]
 // Output: 18
